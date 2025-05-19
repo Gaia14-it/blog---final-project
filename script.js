4ae30053590928e6e881a13a8825867fef7cfe8f
gaia14-it <gaiamaria14n@gmail.com>
2025-05-19 11:42:53 -0400

    sending stuff

---
+++ script.js
@@ -0,0 +1,16 @@
+let imgs = document.querySelectorAll("img")
+
+imgs.forEach(img => img.addEventListener("click", function(ev) {
+    let imgId = ev.target.dataset.imgid;
+    console.log(imgId)
+    // return `../cedited/${imgId}`
+    let newImg = document.createElement("img")
+    newImg.style.position = 'absolute';
+    newImg.style.left = '75px';
+    newImg.style.top = '50px';
+    newImg.src = `cedited/${imgId}`;
+
+    let parent = document.querySelector(`.side`)
+    parent.appendChild(newImg)
+}
+))
